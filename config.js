ea868f451219ec60ec77e1dcb45914f0f365fcd2
hkkrayavo <62047065+hkkvo@users.noreply.github.com>
2022-03-13 14:53:46 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+// Firebase Config
+const firebaseConfig = {
+  apiKey: 'AIzaSyD44qsO8K9mu2nZwCEFEdck8WqysRsxGwU',
+  authDomain: 'templatecharmono.firebaseapp.com',
+  projectId: 'templatecharmono',
+  storageBucket: 'templatecharmono.appspot.com',
+  messagingSenderId: '323043323926',
+  appId: '1:323043323926:web:cf039716006a505f7e0537',
+  measurementId: 'G-DQXQHMKW9G',
+};
+// Exports
+module.exports = firebaseConfig;
